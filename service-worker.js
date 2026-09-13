a1911ca7c5330de4043fe02078bf65b393c77101
jesdeltoro <jjse79@gmail.com>
2026-09-13 21:10:52 +0200

    fix: aumentar volumen del tono de afinación

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'afinador-ukelele-v1';
+const CACHE_NAME = 'afinador-ukelele-v2';
 const ASSETS = ['./', './index.html', './afinador.html', './manifest.webmanifest', './icon.svg'];
 
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(Promise.all([
+  caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))),
+  self.clients.claim()
+])));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
