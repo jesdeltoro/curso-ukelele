331b034511db3a9a7b757aeddc062419e6994dea
jesdeltoro <jjse79@gmail.com>
2026-09-13 21:14:09 +0200

    fix: reforzar volumen del tono de afinación

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'afinador-ukelele-v2';
+const CACHE_NAME = 'afinador-ukelele-v3';
 const ASSETS = ['./', './index.html', './afinador.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
